b3a82f40f656afbb9208284a76397277a5145cbf
Nick Melanson <nickm890@gmail.com>
2022-02-18 23:08:18 -0500

    JS logic for coloring backgrounds based on time done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,35 @@
+// Global variables
 var currentDay = $("#currentDay");
 
+var currentTime = moment();
+
 // Time display using Moment.js
-$("#currentDay").text(moment().format("dddd, MMMM Do"));
+$("#currentDay").text(currentTime.format("dddd, MMMM Do"));
+
+// Time tracker to change block color based on time comparison
+function timeTracker() {
+  // Get current hour number
+  var timeNow = moment().hour();
+
+  // Loop over blocks
+  $(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+
+    // Check time and add/remove classes for background colors
+    if (blockHour < timeNow) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (blockHour === timeNow) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  });
+}
+
+timeTracker();
